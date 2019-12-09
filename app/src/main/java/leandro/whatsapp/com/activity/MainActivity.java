cc50d64b29caee2c895edc115e4f985e2fd860b3
Leandro94 <reisleandro868@gmail.com>
2019-12-09 16:01:04 -0300

    Utilizando abas.

--- app/src/main/java/leandro/whatsapp/com/activity/MainActivity.java
+++ app/src/main/java/leandro/whatsapp/com/activity/MainActivity.java
@@ -3,6 +3,8 @@ package leandro.whatsapp.com.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,9 +12,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 import leandro.whatsapp.com.R;
 import leandro.whatsapp.com.config.ConfiguracaoFirebase;
+import leandro.whatsapp.com.fragment.ContatosFragment;
+import leandro.whatsapp.com.fragment.ConversasFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +36,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
+
+        //Configurar abas
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(   this)
+                .add("Conversas", ConversasFragment.class)
+                .add("Contatos", ContatosFragment.class)
+                .create()
+        );
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout smartTabLayout = findViewById(R.id.viewPagerTab);
+        smartTabLayout.setViewPager(viewPager);
+
     }
 
     @Override
